a8521940f60dafe8eb1e5b59189249c8109248d8
sahil1w223 <sahiljal751@gmail.com>
2026-04-28 21:04:35 +0530

    Update simple_linear_regression.py

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -15,13 +15,14 @@ plt.xlabel('Weight')
 plt.ylabel('Height')
 plt.show()
 
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y, test_size = 0.25, random_state = 42)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y, test_size = 0.25, random_state = 42)
 
 plt.scatter(x_train,y_train)
 
